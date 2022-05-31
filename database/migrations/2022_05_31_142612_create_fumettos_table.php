c1c7aca5bd3706eefa692aa1fa77f05dad26458c
AlbertoBiga <alberto.biga10@gmail.com>
2022-05-31 16:52:55 +0200

    imposto colonne db

--- database/migrations/2022_05_31_142612_create_fumettos_table.php
+++ database/migrations/2022_05_31_142612_create_fumettos_table.php
@@ -15,6 +15,13 @@ class CreateFumettosTable extends Migration
     {
         Schema::create('fumettos', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('thumb');
+            $table->decimal('price', 6, 2);
+            $table->string('series');            
+            $table->date('sale_date');            
+            $table->string('type');
             $table->timestamps();
         });
     }
